4d5e6f0c0613d860ee636a2b7035b02b912557ae
tfermoso <tomas.fermoso@gmail.com>
2025-02-24 20:12:27 +0100

    iniciamos session

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -13,9 +13,11 @@ class LoginController extends Controller{
         if(isset($_POST["username"])){
             $user=User::where("username",$_POST["username"])->first();
             if($user && password_verify($_POST["password"],$user->password)){
+                session_start();
                 $_SESSION["user_id"]=$user->user_id;
                 $_SESSION["username"]=$user->username;
-                
+                header("Location: ".base_url()."admin");
+
             }else{
                 $error="User or pass incorrect";
                 $this->view("login",[$error]);
@@ -33,17 +35,6 @@ class LoginController extends Controller{
         $this->view("register");
     }
 
-    public function json(){
-        $actores=Actor::where("first_name","like","P%")->get();
-        $datos=[
-            "mensaje"=>"Listado actores empiezan P",
-            "listado"=>$actores
-        ];
-        $json=json_encode($datos);
-        header('Content-Type: application/json');
-        echo $json;
-        exit();
-    }
 
 }
 ?>
