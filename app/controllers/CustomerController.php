cdf70296146971fe5e4139e79cd51d1020367bdf
tfermoso <tomas.fermoso@gmail.com>
2025-02-25 18:04:22 +0100

    customerController

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -4,9 +4,9 @@ use Formacom\Core\Controller;
 use Formacom\Models\Customer;
 use Formacom\Models\Address;
 
-class AdminController extends Controller{
+class CustomerController extends Controller{
     public function index(...$params){
-        $customer=Customer::find(1);
+    
         $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
         $this->view('home', $data);
     }
--- app/models/Provider.php
+++ app/models/Provider.php
@@ -5,10 +5,10 @@ class Provider extends Model{
     protected $table="provider";
     protected $primaryKey = 'provider_id';
     public function addresses(){
-        return $this->hasMany(Address::class,"customer_id");
+        return $this->hasMany(Address::class,"provider_id");
     }
     public function phones(){
-        return $this->hasMany(Phone::class,"customer_id");
+        return $this->hasMany(Phone::class,"provider_id");
     }
 }
 
